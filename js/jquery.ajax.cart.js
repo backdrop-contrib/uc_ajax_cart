16c8c442ba47885daf5ad6456bce0249b6060c48
Erik Seifert <erikseifert@161429.no-reply.drupal.org>
2008-11-19 13:51:32 +0000

    *** empty log message ***

--- js/jquery.ajax.cart.js
+++ js/jquery.ajax.cart.js
@@ -4,6 +4,7 @@ jQuery(document).ready(function(){
 	var disabled = Drupal.settings.uc_ajax_cart.disable ;
 	var bclass = Drupal.settings.uc_ajax_cart.bclass ;
 	var ddSupport = Drupal.settings.uc_ajax_cart.ddSupport;
+	var emptyHide = Drupal.settings.uc_ajax_cart.emptyHide;
 	if ( ddSupport == true )
 	{
 		//Activate drag and drop support
@@ -16,13 +17,7 @@ jQuery(document).ready(function(){
 					var i = ev.target.id.split("_").reverse().shift();
 					$form = jQuery('#uc-product-add-to-cart-form-' + i);
 					var data = jQuery($form).formToArray();
-					jQuery($form).ajaxSubmit({
-						success : updateAjaxCart,
-						data : data,
-						async : true,
-						dataType : 'json',
-						url : Drupal.settings.base_path + 'cart/ajax/update'
-					});
+					jQuery.getJSON( Drupal.settings.base_path + 'cart/ajax/update',data,updateAjaxCart);
 					return true ;
 			}
 		});
@@ -41,40 +36,36 @@ jQuery(document).ready(function(){
 									   opacity : 1 });
 			}
 		}
-		jQuery(this).find('input.ajax-submit-form,button.ajax-submit-form').bind('click',function(){
+		jQuery(this).find('input.ajax-submit-form,button.ajax-submit-form').bind('click',function(e){
 				var $form = jQuery(this).parents('form').eq(0);
 				var data = jQuery($form).formToArray();
-					var tagName = this.tagName ;
-					var button = jQuery(this);
-					if ( bText != false )
+				data.push({name: 'emptyHide', value: emptyHide});
+				data.push({name: 'iCount', value:  Drupal.settings.uc_ajax_cart.itemCount });
+				var tagName = this.tagName ;
+				var button = jQuery(this);
+				if ( bText != false )
+				{
+					if ( tagName == "BUTTON" )
 					{
-						if ( tagName == "BUTTON" )
-						{
-						 	button.attr('oldTitle',button.html());
-						 	button.html(bText);
-						}
-						else
-						{
-							 button.attr('oldTitle',button.attr("value"));
-							 button.attr('value',bText);
-						}
+					 	button.attr('oldTitle',button.html());
+					 	button.html(bText);
 					}
-					button.addClass(bclass);
-					if ( disabled == 1 )
+					else
 					{
-						button.css({display : 'none'});
-						button.after('<div class="ajax-cart-msg">' + bText + '</div>');
+						 button.attr('oldTitle',button.attr("value"));
+						 button.attr('value',bText);
 					}
-
-				jQuery($form).ajaxSubmit({
-					success : updateAjaxCart,
-					data : data,
-					async : true,
-					dataType : 'json',
-					url : Drupal.settings.base_path + 'cart/ajax/update'
-				});
-		return false;});
-	})
+				}
+				button.addClass(bclass);
+				if ( disabled == 1 )
+				{
+					button.css({display : 'none'});
+					button.after('<div class="ajax-cart-msg">' + bText + '</div>');
+				}
+				jQuery.getJSON( Drupal.settings.base_path + 'cart/ajax/update',data,updateAjaxCart);
+				return false;
+		});
+	});
 })
 function updateAjaxCart(data,responseType)
 {
@@ -83,11 +74,9 @@ function updateAjaxCart(data,responseType)
 	var bText = Drupal.settings.uc_ajax_cart.text ;
 	var bclass = Drupal.settings.uc_ajax_cart.bclass ;
 	var effects = Drupal.settings.uc_ajax_cart.effects;
-	if ( typeof collapsed_block != "undefined"
-	     && collapsed_block == true)
-	{
-		cart_block_toggle();
-	}
+	var emptyHide = Drupal.settings.uc_ajax_cart.emptyHide;
+	var iCount = Drupal.settings.uc_ajax_cart.itemCount;
+	var aCount = data.itemCount ;
 	jQuery('#' + form_id).find('div.ajax-cart-msg').remove();
 	jQuery('#' + form_id).find('input.ajax-submit-form,button.ajax-submit-form').show().removeClass(bclass).removeAttr('disabled').each(function(){
 		if ( bText != false )
@@ -96,10 +85,28 @@ function updateAjaxCart(data,responseType)
 			else jQuery(this).html(jQuery(this).attr('oldTitle'));
 		}
 	});
+	if ( data.renderAll == true )
+	{
+		$('.cart-block-toggle').unbind('click');
+		$uEle.attr('ID','#ajaxCartUpdate2');
+		$uEle.parent().hide().after(data.content);
+		$uEle.parent().remove();
+		$('.cart-block-toggle').click(function() { cart_block_toggle(); } );
+	} else {
+		$uEle.empty().html(data.content);
+	}
+	Drupal.settings.uc_ajax_cart.itemCount = aCount ;
+	if ( typeof collapsed_block != "undefined"
+	     && collapsed_block == true )
+	{
+		isrc = $('#block-cart-title-arrow').attr('src');
+		if (isrc.toLowerCase().match("up") != null) {
+		    $('#block-cart-title-arrow').attr('src', uc_cart_path + '/images/bullet-arrow-down.gif');
+		}
+	}
 	if ( effects == true )
 	{
 		jQuery('#ajaxCartUpdate').effect('highlight',{},500);
 	}
-	$uEle.empty().html(data.content);
 	jQuery('body').css({cursor : 'default'});
 }
